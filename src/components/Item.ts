283b87f618a9bcf79ca181c7b6f3f4d1bb1834a7
ryumhan <ryum7han@gmail.com>
2022-04-06 11:40:29 +0900

    Duplicated input element appending problem is fixed

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -14,25 +14,10 @@ function createItem(type: string): HTMLElement {
 
 export const getItem = (name: string, input: string) => {
   const item = createItem("normal-item");
+  item.append(name);
 
-  const child = <HTMLInputElement>(
-    item.getElementsByTagName("input")[0].cloneNode(true)
-  );
+  const child = <HTMLInputElement>item.getElementsByTagName("input")[0];
   child.value = name;
 
-  item.append(name, child);
   return item;
-};
-
-const getNormalItem = (name: string) => {
-  return `<div>${name}
-              <input type = "hidden" value = \'${name}\'/>
-          </div>`;
-};
-
-const getItemWithStrong = (name: string, input: string, subs: string) => {
-  return `<div name = ${name}>
-            <strong style = "color : blue">${input}</strong>${subs}
-            <input type = "hidden" value = \'${input + subs}\'/>
-          </div>`;
 };
